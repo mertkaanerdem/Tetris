04eded12aa9bc2a39f3fdff2ed9e4df9ac9e7160
mertkaanerdem <mmertkaanerdem@gmail.com>
2022-01-07 23:07:45 +0300

    splice, concat and appendchild methods

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const width = 10;
   let nextRandom = 0;
   let timerId;
+  let score = 0;
 
   //The Tetrominoes
   const lTetromino = [
@@ -114,6 +115,7 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition = 4;
       draw();
       displayShape();
+      addScore();
     }
   }
   //https://keycode.info/
@@ -208,4 +210,34 @@ document.addEventListener("DOMContentLoaded", () => {
       displayShape();
     }
   });
+
+  //add score
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [
+        i,
+        i + 1,
+        i + 2,
+        i + 3,
+        i + 4,
+        i + 5,
+        i + 6,
+        i + 7,
+        i + 8,
+        i + 9,
+      ];
+
+      if (row.every((index) => squares[index].classList.contains("taken"))) {
+        score += 33;
+        ScoreDisplay.innerHTML = score;
+        row.forEach((index) => {
+          squares[index].classList.remove("taken");
+          squares[index].classList.remove("tetromino");
+        });
+        const squaresRemoved = squares.splice(i, width);
+        squares = squaresRemoved.concat(squares);
+        squares.forEach((cell) => grid.appendChild(cell));
+      }
+    }
+  }
 });
