574a919de4444df7186bb9e500ef0ea30ff734fa
mertkaanerdem <mmertkaanerdem@gmail.com>
2022-01-07 21:35:26 +0300

    keyCodes and events with move functions

--- app.js
+++ app.js
@@ -73,6 +73,21 @@ document.addEventListener("DOMContentLoaded", () => {
   //make the tetromino move down every second
   timerId = setInterval(moveDown, 1000);
 
+  //assign function to keyCodes
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 38) {
+      //rotate()
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+  document.addEventListener("keyup", control);
+
+  //move down function
   function moveDown() {
     undraw();
     currentPosition += width;
@@ -96,5 +111,46 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition = 4;
       draw();
     }
+  }
+  //https://keycode.info/
+
+  //move the tetromino left, unless is at the edge or there is a blockage
+  //usign in control function
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+
+    if (!isAtLeftEdge) currentPosition -= 1;
+
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+
+  //move the tetromino right, unless is at the edge or there is a blockage
+  //usign in control function
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(
+      (index) => (currentPosition + index) % width === width - 1
+    );
+
+    if (!isAtRightEdge) currentPosition += 1;
+
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition -= 1;
+    }
+    draw();
   }
 });
