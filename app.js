77d2de0b8863af8369fffda1120b10884a95d5d9
mertkaanerdem <mmertkaanerdem@gmail.com>
2022-01-07 17:26:54 +0300

    setting variables

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const ScoreDisplay = document.querySelector("#score");
+  const StartButton = document.querySelector("#start-button");
+  const width = 10;
+
+  //   function alertMe() {
+  //     alert("Hi! Welcome to the game!");
+  //   }
+  //   alertMe();
+});
