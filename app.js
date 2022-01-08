68c17efab41377f1ea9efaf53f4cdb10b226bef6
mertkaanerdem <mmertkaanerdem@gmail.com>
2022-01-08 22:09:04 +0300

    styling

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let nextRandom = 0;
   let timerId;
   let score = 0;
-  const colors = ["orange", "red", "purple", "green", "blue"];
+  const colors = ["#ff9900", "#002663", "#3be8b0", "#ff322e", "#a626aa"];
 
   //The Tetrominoes
   const lTetromino = [
